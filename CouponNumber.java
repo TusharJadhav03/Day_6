b701a4ee38cdf4c506ae849c16af650f98edb4c4
TUSHAR JADHAV <160405308+TusharJadhav03@users.noreply.github.com>
2024-03-28 11:39:35 +0530

    File Updated

---
+++ CouponNumber.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CouponNumber {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        ArrayList<Integer> a = new ArrayList<>();
+        a = rand.nextInt(200);
+    }
+}
